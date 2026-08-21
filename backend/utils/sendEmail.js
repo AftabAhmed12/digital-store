372952ea3467ddb787377f0d0b9ba9fb9818186f
Aftab Ahmed <aftab.ahmed@zaxiss.com>
2026-08-21 18:41:19 +0500

    Added the nodemailer instead of resend

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,12 +1,20 @@
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT) || 587,
+  secure: process.env.SMTP_SECURE === "true",
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
 export const sendEmail = async ({ to, subject, html }) => {
   try {
-    const response = await resend.emails.send({
+    const response = await transporter.sendMail({
       from: process.env.EMAIL_FROM,
       to,
       subject,
